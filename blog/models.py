e5e90f24eb2e35c3e5c5675eb6d84c211e0e0c4e
Wwle <weiyuecn5@gmail.com>
2019-11-14 16:08:52 +0800

    11.14修改1

--- blog/models.py
+++ blog/models.py
@@ -5,15 +5,39 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     # 文章标题
     title = models.CharField(max_length=70)
-    # 文章正文，我们使用了 TextField。
-    # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
     body = models.TextField()
     # 这两个列分别表示文章的创建时间和最后一次修改时间，存储时间的字段用 DateTimeField 类型。
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
-    # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
+    # 文章作者
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Hot(models.Model):
-    #热门宠物
     name = models.CharField(max_length=100)
+    number=models.CharField(max_length=50,default=0)
+
+class shujuku(models.Model):
+    账号编号=models.CharField(max_length=100,primary_key=True)
+    石头数量=models.CharField(max_length=100,blank=True)
+    等级=models.CharField(max_length=100,blank=True)
+    更新时间=models.CharField(max_length=100,blank=True)
+    宠物=models.TextField()
+    已卖=models.CharField(max_length=20,default='是')
+    买家=models.CharField(max_length=100,blank=True)
+    价格=models.CharField(max_length=100,blank=True)
+
+class duizhao(models.Model):
+    宠物编号=models.CharField(max_length=100,primary_key=True)
+    宠物名字=models.CharField(max_length=100,blank=True)
+    宠物价值 = models.CharField(max_length=100, blank=True)
+
+class huancun(models.Model):
+    #缓存数据库 存放新加入的数据
+    #唯一键=账号编号-石头数量-宠物-时间
+    唯一键=models.CharField(max_length=150,primary_key=True)
+    账号编号=models.CharField(max_length=100)
+    石头数量=models.CharField(max_length=100,blank=True)
+    等级=models.CharField(max_length=100,blank=True)
+    宠物=models.TextField()
+    是否上传=models.CharField(max_length=2,default='否')
+    更新时间=models.CharField(max_length=100,blank=True)
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import Post,Hot
 
 def index(request):
-    post_list=Post.objects.all()
-    return render(request, 'blog/index.html', context={
-        'post_list':post_list
+    post_list=Post.objects.all().order_by('-created_time')
+    hot_list = Hot.objects.all().order_by('number')
+    return render(request, 'blog/base.html', context={
+        'post_list':post_list,
+        'hot_list':hot_list
     })
