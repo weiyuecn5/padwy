8f6cfa4572555ccc58e7bb752862fdb24180b9c0
Wwle <weiyuecn5@gmail.com>
2019-11-15 16:05:08 +0800

    11.15更新代码

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post, Hot
+from .models import Post, Hot,shujuku,duizhao
 
 admin.site.register(Post)
 admin.site.register(Hot)
+admin.site.register(shujuku)
+admin.site.register(duizhao)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Post,Hot
 
 def index(request):
     post_list=Post.objects.all().order_by('-created_time')
     hot_list = Hot.objects.all().order_by('number')
-    return render(request, 'blog/base.html', context={
+    return render(request, 'blog/index.html', context={
         'post_list':post_list,
         'hot_list':hot_list
+    })
+def index1(request):
+    post_list=Post.objects.all().order_by('-created_time')
+    paginator = Paginator(post_list,2) #每页显示2篇
+    page= request.GET.get('page',1)
+    posttomer=paginator.page(page)
+    return render(request,'blog/index1.html',context={
+        'post_list':posttomer,
     })
