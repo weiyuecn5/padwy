fccd180e0d7797ae0873732b9f2191be562be1c0
Wwle <weiyuecn5@gmail.com>
2020-01-10 13:45:54 +0800

    1.10更新代码-3

--- padwy/settings.py
+++ padwy/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_ROOT = '/home/weile/sites/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_new')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
