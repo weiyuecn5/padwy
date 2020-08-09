c0772cebfe4e968edb855a4ee726001f024cb29c
weile <weiyuecn5@gmail.com>
2020-08-09 09:41:38 +0800

    8-9-2

--- pais.py
+++ pais.py
@@ -6,6 +6,38 @@ import sqlite3
 
 conn = sqlite3.connect('db.sqlite3')  # 链接数据库
 c = conn.cursor()
-c.execute("SELECT * from blog_shujuku")
-c.execute("UPDATE blog_shujuku set 买家 = 'Fire'")
-conn.commit()
+
+a=int(input('开始编号:'))
+b=int(input('结束编号:'))
+
+def ptj(number):
+
+    full_url = 'http://pad.skyozora.com/pets/' + str(number)
+
+    try:
+        r = requests.get(full_url)
+        soup = BeautifulSoup(r.text, features="html.parser")
+        b = soup.title.text.split('-')
+        bb = soup.find('span', class_='yellow bold')
+        cwbh=b[0].strip()
+        cwmz=b[1].strip()
+        cwjz=bb.text
+        print(cwbh,cwmz,cwjz)
+        t=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        sql = "INSERT INTO blog_duizhao (宠物编号,宠物名字,宠物价值,加入时间) VALUES ('%s','%s','%s','%s')" % (cwbh, cwmz, cwjz,t)
+        c.execute(sql)
+        conn.commit()
+    except:
+        pass
+
+for i in range(a, b+ 1):
+    c.execute("SELECT 宠物名字 FROM blog_duizhao WHERE 宠物编号='%s'" % (str(i)))
+    data = c.fetchone()
+    print(data)
+    if data==None:
+        ptj(i)
+        print(i)
+        sleep(2)
+    else:
+        print('数据存在:'+str(i))
+conn.close()
